9fab9b88020cca186602690a9ad24fbc2ae011ee
Andrew Chang <89715746+itsmandrew@users.noreply.github.com>
2024-04-17 10:21:54 -0700

    Separation of JS from html and also fixed title

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+
+var typed = new Typed(".type1", {
+    strings: [
+      'npm install^1000 <br>`installing dependencies...` ^1000 <br>`Fetching from source...` <br>> Hello, this is Andrew :D <br><br><a href="javascript:void(0);" onclick="jumpToSection(\'about\')" class="purple">about</a>\t <a href="javascript:void(0);" onclick="jumpToSection(\'experience\')" class="purple">experience</a>\t <a href="javascript:void(0);" onclick="jumpToSection(\'contact\')" class="purple">contact</a>',
+    ],
+    typeSpeed: 30, // typing speed
+    backDelay: 750, // pause before backspacing
+    loop: false, // loop on or off (true or false)
+    loopCount: false, // number of loops, false = infinite
+  });
+
+  var typed2 = new Typed(".type2", {
+    strings: ["life", "skills", "hobbies"],
+    typeSpeed: 80, // typing speed
+    backSpeed: 80,
+    backDelay: 750, // pause before backspacing
+    loop: true, // loop on or off (true or false)
+    loopCount: false, // number of loops, false = infinite
+  });
+
+
+  var typed3 = new Typed(".type3", {
+    strings: ["experience", "jobs", "career"],
+    typeSpeed: 80, // typing speed
+    backSpeed: 80,
+    backDelay: 750, // pause before backspacing
+    loop: true, // loop on or off (true or false)
+  });
+
+
 function jumpToSection(sectionId) {
     let element = document.getElementById(sectionId);
     if (element) {
