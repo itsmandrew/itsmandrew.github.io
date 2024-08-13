a5f18331d8fd75848f4a02bd882f57119dda47ed
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-08-12 20:47:28 -0700

    Optimizing load, smoother transitions in

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
-
 var typed = new Typed(".type1", {
-    strings: [
-      'npm install^1000 <br>`installing dependencies...` ^1000 <br>`Fetching from source...` <br>> Hello, this is Andrew :D <br><br><a href="javascript:void(0);" onclick="jumpToSection(\'about\')" class="purple">about</a>\t <a href="javascript:void(0);" onclick="jumpToSection(\'experience\')" class="purple">experience</a>\t <a href="javascript:void(0);" onclick="jumpToSection(\'contact\')" class="purple">contact</a>',
-    ],
-    typeSpeed: 30, // typing speed
-    backDelay: 750, // pause before backspacing
-    loop: false, // loop on or off (true or false)
-    loopCount: false, // number of loops, false = infinite
-  });
+  strings: [
+    'npm install^1000 <br>`installing dependencies...` ^1000 <br>`Fetching from source...` <br>> Hello, this is Andrew :D <br><br><a href="javascript:void(0);" onclick="jumpToSection(\'about\')" class="purple">about</a>\t <a href="javascript:void(0);" onclick="jumpToSection(\'experience\')" class="purple">experience</a>\t <a href="javascript:void(0);" onclick="jumpToSection(\'contact\')" class="purple">contact</a>',
+  ],
+  typeSpeed: 30, // typing speed
+  backDelay: 750, // pause before backspacing
+  loop: false, // loop on or off (true or false)
+  loopCount: false, // number of loops, false = infinite
+});
 
-  var typed2 = new Typed(".type2", {
-    strings: ["life", "skills", "hobbies"],
-    typeSpeed: 80, // typing speed
-    backSpeed: 80,
-    backDelay: 750, // pause before backspacing
-    loop: true, // loop on or off (true or false)
-    loopCount: false, // number of loops, false = infinite
-  });
+var typed2 = new Typed(".type2", {
+  strings: ["life", "skills", "hobbies"],
+  typeSpeed: 80, // typing speed
+  backSpeed: 80,
+  backDelay: 750, // pause before backspacing
+  loop: true, // loop on or off (true or false)
+  loopCount: false, // number of loops, false = infinite
+});
 
-
-  var typed3 = new Typed(".type3", {
-    strings: ["experience", "jobs", "career"],
-    typeSpeed: 80, // typing speed
-    backSpeed: 80,
-    backDelay: 750, // pause before backspacing
-    loop: true, // loop on or off (true or false)
-  });
-
+var typed3 = new Typed(".type3", {
+  strings: ["experience", "jobs", "career"],
+  typeSpeed: 80, // typing speed
+  backSpeed: 80,
+  backDelay: 750, // pause before backspacing
+  loop: true, // loop on or off (true or false)
+});
 
 function jumpToSection(sectionId) {
-    let element = document.getElementById(sectionId);
-    if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-        // No need to add 'sliding-section' here as it's handled by the observer
-    }
+  let element = document.getElementById(sectionId);
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth" });
+    // No need to add 'sliding-section' here as it's handled by the observer
+  }
 }
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
 });
 
-const slidingElements = document.querySelectorAll('.hidden, .hidden2');
+const slidingElements = document.querySelectorAll(".hidden, .hidden2");
 slidingElements.forEach((e1) => observer.observe(e1));
